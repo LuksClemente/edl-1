aba6c7ef617c0d389c1f78ff4973e1dfc06f308d
dymbow10 <62061476+dymbow10@users.noreply.github.com>
2020-03-28 13:52:15 -0300

    Add files via upload

---
+++ Tarefa-01/Programas/teste-2.pl
@@ -0,0 +1,18 @@
+print("ADIVINHE O NUMERO\n\n");
+print("Voce tem tres chances e o numero esta entre 0 e 9 :)\n");$n=int(rand(10));
+#print("$n");
+for($i = 0, $i<3, $i+=1){
+	print("Faca sua escolha:\n");
+	$e=<STDIN>;
+	chomp($e);
+	if($e > $n) {
+		print("menor!\n");
+	}
+	elsif($e < $n){
+		print("maior!\n");
+	}
+	else{
+		print("Ganhou!!!!!\n");
+		last;
+	}
+}
