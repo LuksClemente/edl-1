54c16abf12c35bea0c0e4694fc229ef01dd55560
dymbow10 <62061476+dymbow10@users.noreply.github.com>
2020-03-28 13:53:28 -0300

    Rename teste-2.pl to programa-2.pl

--- Tarefa-01/Programas/programa-2.pl
+++ Tarefa-01/Programas/programa-2.pl
@@ -1,5 +1,6 @@
 print("ADIVINHE O NUMERO\n\n");
-print("Voce tem tres chances e o numero esta entre 0 e 9 :)\n");$n=int(rand(10));
+print("Voce tem tres chances e o numero esta entre 0 e 9 :)\n");
+$n=int(rand(10));
 #print("$n");
 for($i = 0, $i<3, $i+=1){
 	print("Faca sua escolha:\n");
